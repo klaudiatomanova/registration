1009de474d15ee0779649d58ab5cadd316c22ddb
Klaudia Tomanova <klaudiatomanova@Klaudias-MacBook-Air.local>
2020-05-19 16:56:57 +0200

    Updates

---
+++ precache-manifest.6a4e09be1aa16119d2fb1fa0bb1138ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db42892b53dba9635b925fb77f70ee7",
+    "url": "/registration/index.html"
+  },
+  {
+    "revision": "cf2abaf09acf9cc27659",
+    "url": "/registration/static/css/main.d5c65a73.chunk.css"
+  },
+  {
+    "revision": "dd47635a7395859e92b1",
+    "url": "/registration/static/js/2.943de1d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/registration/static/js/2.943de1d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2abaf09acf9cc27659",
+    "url": "/registration/static/js/main.af4d215d.chunk.js"
+  },
+  {
+    "revision": "a098dfbe3be5dd9a9e5f",
+    "url": "/registration/static/js/runtime-main.fcfe68c5.js"
+  }
+]);
